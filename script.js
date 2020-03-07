9371ffebabd1f0ee44d0c58cb4531b16bb06609b
Brandon Kim <bkim377@gatech.edu>
2020-03-06 19:17:03 -0500

    Checkpoint 1 - added arrays for password characters, progressing on prompts

--- script.js
+++ script.js
@@ -4,17 +4,41 @@ var generateBtn = document.querySelector("#generate");
 // Generate Password
 function generatePassword(){
   // Prompt the user for password length.
+  passLength = prompt("How many characters will your password be?");
   // Check to see if the user actually entered a length.
+  if(passLength >= 8 && passLength <= 128) {
+    alert("Your password will be " + passLength + " characters long.");
+  } else {
+    alert("Please enter a number between 8 and 128 for the password length.");
+    
+  }
+
   // 4 Confirms
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var specialChars = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  var passArray = [];
+
   // 1) Special Characters
+  specialConfirm = confirm("Do you want to use special characters? (e.g. !#$%&'()*+,-./:;<=>?@[\]^_`{|}~");
+
   // 2) Numbers 
+  numbersConfirm = confirm("Do you want to use numbers?");
+
   // 3) Lowercase
+  lowerCaseConfirm = confirm("Do you want to use lowercase letters?");
+
   // 4) Uppercase
+  upperCaseConfirm = confirm("Do you want to use uppercase letters?");
+
   // Generate password, based on user specifications.
+
+
   // NOTE: User specifications come from confirms.
   // 
   for(var i = 0; i <= 10; i++){
-    
+
   }
   return "Something";
 };
